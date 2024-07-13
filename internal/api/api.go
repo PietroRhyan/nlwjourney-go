c0794ce411885f04efaf1ea7f81ac5aaa95f8cd6
Pietro Rhyan <pietroqjg@gmail.com>
2024-07-13 12:54:35 -0300

    feat: add createTrip endpoint and mailer service

--- internal/api/api.go
+++ internal/api/api.go
@@ -2,34 +2,48 @@ package api
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
-	"journey/internal/api/spec"
-	"journey/internal/pgstore"
 	"net/http"
 
+	"journey/internal/api/spec"
+	"journey/internal/pgstore"
+
+	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
+
 	"go.uber.org/zap"
 )
 
 type store interface {
-	GetParticipant(ctx context.Context, participantID uuid.UUID) (pgstore.Participant, error)
-	ConfirmParticipant(ctx context.Context, participantID uuid.UUID) error
+	GetParticipant(context.Context, uuid.UUID) (pgstore.Participant, error)
+	ConfirmParticipant(context.Context, uuid.UUID) error
+	CreateTrip(context.Context, *pgxpool.Pool, spec.CreateTripRequest) (uuid.UUID, error)
+}
+
+type mailer interface {
+	SendConfirmTripEmailToTripOwner(uuid.UUID) error
 }
 
 type API struct {
-	store  store
-	logger *zap.Logger
+	store     store
+	logger    *zap.Logger
+	validator *validator.Validate
+	pool      *pgxpool.Pool
+	mailer    mailer
 }
 
-func NewAPI(pool *pgxpool.Pool, logger *zap.Logger) API {
-	return API{pgstore.New(pool), logger}
+func NewAPI(pool *pgxpool.Pool, logger *zap.Logger, mailer mailer) API {
+	validator := validator.New(validator.WithRequiredStructEnabled())
+
+	return API{pgstore.New(pool), logger, validator, pool, mailer}
 }
 
 // Confirms a participant on a trip.
 // (PATCH /participants/{participantId}/confirm)
-func (api API) PatchParticipantsParticipantIDConfirm(w http.ResponseWriter, r *http.Request, participantID string) *spec.Response {
+func (api *API) PatchParticipantsParticipantIDConfirm(w http.ResponseWriter, r *http.Request, participantID string) *spec.Response {
 	id, err := uuid.Parse(participantID)
 	if err != nil {
 		return spec.PatchParticipantsParticipantIDConfirmJSON400Response(spec.Error{Message: "uuid invalido"})
@@ -64,8 +78,29 @@ func (api API) PatchParticipantsParticipantIDConfirm(w http.ResponseWriter, r *h
 
 // Create a new trip
 // (POST /trips)
-func (api API) PostTrips(w http.ResponseWriter, r *http.Request) *spec.Response {
-	panic("not implemented") // TODO: Implement
+func (api *API) PostTrips(w http.ResponseWriter, r *http.Request) *spec.Response {
+	var body spec.CreateTripRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		return spec.PostTripsJSON400Response(spec.Error{Message: "invalid JSON: " + err.Error()})
+	}
+
+	if err := api.validator.Struct(body); err != nil {
+		return spec.PostTripsJSON400Response(spec.Error{Message: "invalid input: " + err.Error()})
+	}
+
+	tripID, err := api.store.CreateTrip(r.Context(), api.pool, body)
+	if err != nil {
+		return spec.PostTripsJSON400Response(spec.Error{Message: "failed to create trip, try again"})
+	}
+
+	go func() {
+		if err := api.mailer.SendConfirmTripEmailToTripOwner(tripID); err != nil {
+			api.logger.Error("failed to send email on PostTrips", zap.Error(err), zap.String("trip_id", tripID.String()))
+		}
+	}()
+
+	return spec.PostTripsJSON201Response(spec.CreateTripResponse{TripID: tripID.String()})
 }
 
 // Get a trip details.
